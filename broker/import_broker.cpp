b16c4c47b1bbe9dd713ff0deba4b59d161f1e2c2
Pavel Kirsanov <melstrom83@gmail.com>
2023-09-24 19:37:44 +0300

    Removed pragma

--- broker/import_broker.cpp
+++ broker/import_broker.cpp
@@ -25,8 +25,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ================================================================================*/
-#pragma once
-
 #include "import_broker.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
@@ -52,7 +50,6 @@ THE SOFTWARE.
 #include <mikktspace/mikktspace.h>
 
 #include <xatlas/xatlas.h>
-
 
 namespace RayGene3D
 {
