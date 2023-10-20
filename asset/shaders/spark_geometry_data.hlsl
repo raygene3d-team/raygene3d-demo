60bc3dd5ac34994103f2ba60fdf8b9346ded53bf
Roman Sementsov <sementsovroman@gmail.com>
2023-10-20 20:24:02 +0300

    Revert "fixed review notes"
    This reverts commit 777a8f56e29bb0ffbe87a8d91187d338003801b5.

--- asset/shaders/spark_geometry_data.hlsl
+++ asset/shaders/spark_geometry_data.hlsl
@@ -5,14 +5,14 @@ struct GeometryData
 {
   float3 position_ws;
   float3 normal_ws;
-  float4 tangent_ws;
+  float4 tanghent_ws;
 };
 
 void InitializeGeometryData(inout GeometryData geometry_data, float3 position_ws, float3 normal_ws, float4 tangent_ws)
 {
   geometry_data.position_ws = position_ws;
   geometry_data.normal_ws = normalize(normal_ws);
-  geometry_data.tangent_ws.xyz = normalize(tangent_ws.xyz);
-  geometry_data.tangent_ws.w = tangent_ws.w;
+  geometry_data.tanghent_ws.xyz = normalize(tangent_ws.xyz);
+  geometry_data.tanghent_ws.w = tangent_ws.w;
 }
 #endif
